855846384e5cb6c234ca71f96fb2075cd9a31e7c
defi-seer <sawigigo299@gmail.com>
2025-01-14 01:34:33 +0900

    Create Header.js

---
+++ ui/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>DeFi Seer Dashboard</h1>
+      <nav>
+        <ul>
+          <li><a href="#dashboard">Dashboard</a></li>
+          <li><a href="#insights">Insights</a></li>
+          <li><a href="#settings">Settings</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
