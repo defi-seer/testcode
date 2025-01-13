cf770e84e19c1941d408ad04e352304f62b56daf
defi-seer <sawigigo299@gmail.com>
2025-01-14 01:38:49 +0900

    Update token.test.js

--- tests/unit-tests/token.test.js
+++ tests/unit-tests/token.test.js
@@ -12,4 +12,20 @@ describe('Token Contract', function () {
   });
 
   it('Should assign the total supply to the owner', async function () {
-   
+    const ownerBalance = await token.balanceOf(owner.address);
+    expect(await token.totalSupply()).to.equal(ownerBalance);
+  });
+
+  it('Should transfer tokens between accounts', async function () {
+    await token.transfer(addr1.address, 50);
+    expect(await token.balanceOf(addr1.address)).to.equal(50);
+
+    await token.connect(addr1).transfer(addr2.address, 50);
+    expect(await token.balanceOf(addr2.address)).to.equal(50);
+  });
+
+  it('Should fail if sender doesn’t have enough tokens', async function () {
+    await expect(token.connect(addr1).transfer(addr2.address, 50))
+      .to.be.revertedWith('Insufficient balance');
+  });
+});
